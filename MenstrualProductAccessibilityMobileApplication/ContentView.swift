eb62b1490e5a667eb4083516b7fc42701ca0cf87
navaem <navaem@gmail.com>
2020-03-10 16:20:19 -0400

    Initial Commit

---
+++ MenstrualProductAccessibilityMobileApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenstrualProductAccessibilityMobileApplication
+//
+//  Created by Ed Nava on 3/10/20.
+//  Copyright © 2020 cs4675. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
